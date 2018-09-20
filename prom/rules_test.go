d5b38918c2c46962663e336dbc1b9d45dd54b726
Cody Boggs <cody@freshtracks.io>
2018-09-20 09:01:51 -0600

    lulz, nevermind, snippets were inaccurate. prom.AppendRuleFile is fine, and now tested

--- prom/rules_test.go
+++ prom/rules_test.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/Fresh-Tracks/bomb-squad/bstesting"
 	"github.com/Fresh-Tracks/bomb-squad/config"
+	"github.com/Fresh-Tracks/bomb-squad/prom"
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: The AppendRulesFile function in the prom package is kinda screwy, as it's specific to
-// ConfigMaps. Should refactor it to use Configurators, then do this test properly
 func TestCanAppendRulesFile(t *testing.T) {
 	c := bstesting.NewConfigurator(t)
 	promcfg, err := config.ReadPromConfig(c)
 	require.NoError(t, err)
-	//require.NotEmpty(t, promcfg)
+	promcfg, err = prom.AppendRuleFile("/test/rules/file.yaml", c)
+	require.Equal(t, "/test/rules/file.yaml", promcfg.RuleFiles[1])
 }
